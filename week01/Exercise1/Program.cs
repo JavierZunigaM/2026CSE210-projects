eb7668496e3a5a24240b32e8f1ad20eb75288167
JavierZunigaM <jzm2006@gmail.com>
2026-07-05 16:10:00 -0600

    Add WriteLine for last name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,7 +7,7 @@ class Program
         Console.WriteLine("What is your first name?");
         string firstName = Console.ReadLine();
         
-        Console.Write("What is your last name?");
+        Console.WriteLine("What is your last name?");
         string lastName = Console.ReadLine();
 
         Console.WriteLine();
