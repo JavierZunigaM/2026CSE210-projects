cb7e4c6f9f487f8f50857a829be599e4fb6709b2
JavierZunigaM <jzm2006@gmail.com>
2026-07-13 12:14:51 -0600

    message

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         //Main program logic
-        Console.WriteLine("Hello World! This is the Journal Project Starting.");
+        Console.WriteLine("Hello World! This is the Journal Project Starting...");
     }
 }
