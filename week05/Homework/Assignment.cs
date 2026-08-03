1ececddf5747d164ea449504df03d49402edfc9c
JavierZunigaM <jzm2006@gmail.com>
2026-08-03 13:21:53 -0600

    W05 Learning Activity: Inheritance

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,22 @@
+public class Assignment
+{
+    private string _studentName;
+    private string _topic;
+
+public Assignment(string studentName, string topic)
+    {
+        _studentName = studentName;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} has submitted an assignment on {_topic}.";
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+    
+}
