792e339761087a8718fdf1866b182ca66dcd5323
JavierZunigaM <jzm2006@gmail.com>
2026-08-06 15:55:31 -0600

    W6 Shapes

---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Shapes
+{
+    public abstract class Shape
+    {
+        private string _color;
+        
+        public Shape (string color)
+        {
+            _color = color;
+        }
+
+        public string GetColor()
+        {
+            return _color;
+        }
+        public abstract double GetArea();
+
+    }
+}
