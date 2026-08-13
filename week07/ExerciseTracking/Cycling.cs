23722221f556d5b60b870e845bf82ecb4344c577
JavierZunigaM <jzm2006@gmail.com>
2026-08-13 11:45:32 -0600

    WW7 assignment

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,20 @@
+public class Cycling : Activity
+{
+    public double _speedKph; 
+    public Cycling (string date, int minutes, double speedKph) : base (date, minutes)
+    {
+        _speedKph = speedKph;
+    }
+    public override double GetDistance()
+    {
+        return _speedKph * (GetMinutes() / 60.0);
+    }
+    public override double GetSpeed()
+    {
+        return _speedKph;
+    }
+    public override double GetPace()
+    {
+        return 60 / _speedKph;
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List <Activity> activities = new List<Activity>();
+        activities.Add(new Running("03 Nov 2022", 30, 4.8)); // km
+        activities.Add(new Cycling("03 Nov 2022", 45, 20.0)); // kph
+        activities.Add(new Swimming("03 Nov 2022", 25, 30)); // Laps
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
